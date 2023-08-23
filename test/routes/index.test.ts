d9ebeff3b89acd827f16b6ad1e41984df6e1aa9d
Damien Heulin <damienheulin87@gmail.com>
2023-08-23 14:51:58 +0200

    feat: add a setup test function to build and start the server during tests

--- test/routes/index.test.ts
+++ test/routes/index.test.ts
@@ -1,7 +1,16 @@
+import { setupTests } from "../tests";
 import { describe, expect, test } from "vitest";
 
-describe("Index route", () => {
-    test("should pass", () => {
-        expect(true).toBe(true);
+describe("index route", async () => {
+    const ctx = await setupTests();
+
+    test("returns a 200", async () => {
+        const { status } = await ctx.fetch("/");
+        expect(status).toBe(200);
+    });
+
+    test("returns { nitro: Is Awesome }", async () => {
+        const response = await ctx.fetch("/");
+        expect(await response.json()).toMatchObject({ nitro: "Is Awesome!" });
     });
 });
